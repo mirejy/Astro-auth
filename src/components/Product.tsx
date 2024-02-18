8a69579ca8d215c5dbee2a569f9b9091fa6c4e09
Mihail <allo124558@gmail.com>
2024-02-19 00:09:48 +0100

    add destructing and style at button

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ title, price, image, description }) {
   const [openDescription, setOpenDescription] = useState(false);
 
   return (
     <div className="border rounded mb-2 p-3">
-      <p>{product.title}</p>
-      <p>{product.price}</p>
-      <img width={200} height={200} src={product.image} alt="" />
-      {openDescription && <p>{product.description}</p>}
+      <p>{title}</p>
+      <p>{price}</p>
+      <img width={200} height={200} src={image} alt="" />
+      {openDescription && <p>{description}</p>}
 
       <button onClick={() => setOpenDescription(!openDescription)}>
-        Show description
+        {!openDescription ? "↓" : "↑"}
       </button>
     </div>
   );
